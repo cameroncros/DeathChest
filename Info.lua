ca80f9f136d2352984f3d16db108800623681632
BuildTools <unconfigured@null.spigotmc.org>
2016-06-27 23:27:23 +0000

    Added Info.lua

---
+++ Info.lua
@@ -0,0 +1,13 @@
+
+-- Info.lua
+
+-- Implements the g_PluginInfo standard plugin description
+g_PluginInfo = 
+{
+	Name = "DeathChest",
+	Version = "0.1",
+	Date = "2016-06-28",
+	SourceLocation = "https://github.com/cameroncros/DeathChest",
+	Description = [[A plugin for Cuberite that places a players items into a chest when they die]]
+}
+
